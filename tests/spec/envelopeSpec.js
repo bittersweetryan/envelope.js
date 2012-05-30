da2fd3d299fad3064135601f25121cc88b55192a
Ryan Anklam <ryan.anklam@gmail.com>
2012-05-30 08:00:36 -0500

    renamed envelope to show version, added spec and fixture

---
+++ tests/spec/envelopeSpec.js
@@ -0,0 +1,13 @@
+describe("envelope",function(){
+	loadFixtures('envelopeFixture.html');
+
+	beforeEach(function(){
+		$("#messages").envelope(
+			{uiFramework : 'jQueryUI'}
+		);
+	});
+
+	it("should add a message on an event",function(){
+
+	});
+});
