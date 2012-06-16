7e7583c14aa7593384810cefdb56523cc23b52f6
Ryan Anklam <ryan.anklam@gmail.com>
2012-06-15 21:08:15 -0500

    finished try it out section

--- javascripts/createAlerts.js
+++ javascripts/createAlerts.js
@@ -2,7 +2,7 @@ $(function(){
 	prettyPrint();
 
 	var $messageContainer_bootstrap = $("#messages_bootstrap");
-	var $messageContainer_ui = $("#messages_ui");
+	var $messageContainer_jqueryui = $("#messages_jqueryui");
 	var $messageContainer_default = $("#messages_default");
 
 	$messageContainer_bootstrap.envelope(
@@ -12,26 +12,119 @@ $(function(){
 		},
 		[
 			{
-				name : 'bootstrap.save',
-				message: "You just clicked the save button.",
+				name : 'bootstrap.success',
+				message: "The operation was a success!",
+				callback: function(){
+				//	$(this).trigger("callback.fired");
+				},
+				autoClose: true,
+				addCloseButton: true,
+				type: 'success'
+			},
+			{
+				name : 'bootstrap.error',
+				message: "There was an error!",
+				callback: function(){
+				//	$(this).trigger("callback.fired");
+				},
+				autoClose: true,
+				addCloseButton: true,
+				type: 'error'
+			},
+			{
+				name : 'bootstrap.info',
+				message: "Something happened!",
 				callback: function(){
 				//	$(this).trigger("callback.fired");
 				},
 				autoClose: true,
 				addCloseButton: true,
 				type: 'info'
-			},
-			{
-				name : 'callback.fired',
-				message: "The callback just fired.",
-				autoClose: true,
-				addCloseButton: true,
-				type: 'success'
 			}
 		]
 	);
 
-	$("#trigger_save").on('click',function(){
-		$(this).trigger('bootstrap.save');
+	$messageContainer_jqueryui.envelope(
+		{
+			uiFramework : 'jqueryui',
+			autoCloseTimeout : 5000
+		},
+		[
+			{
+				name : 'jqueryui.success',
+				message: "The operation was a success!",
+				callback: function(){
+				//	$(this).trigger("callback.fired");
+				},
+				autoClose: true,
+				addCloseButton: true,
+				type: 'success'
+			},
+			{
+				name : 'jqueryui.error',
+				message: "There was an error!",
+				callback: function(){
+				//	$(this).trigger("callback.fired");
+				},
+				autoClose: true,
+				addCloseButton: true,
+				type: 'error'
+			},
+			{
+				name : 'jqueryui.info',
+				message: "Something happened!",
+				callback: function(){
+				//	$(this).trigger("callback.fired");
+				},
+				autoClose: true,
+				addCloseButton: true,
+				type: 'info'
+			}
+		]
+	);
+
+	$messageContainer_default.envelope(
+		{
+			autoCloseTimeout : 5000
+		},
+		[
+			{
+				name : 'default.success',
+				message: "The operation was a success!",
+				callback: function(){
+				//	$(this).trigger("callback.fired");
+				},
+				autoClose: true,
+				addCloseButton: false,
+				type: 'success'
+			},
+			{
+				name : 'default.error',
+				message: "There was an error!",
+				callback: function(){
+				//	$(this).trigger("callback.fired");
+				},
+				autoClose: true,
+				addCloseButton: false,
+				type: 'error'
+			},
+			{
+				name : 'default.info',
+				message: "Something happened!",
+				callback: function(){
+				//	$(this).trigger("callback.fired");
+				},
+				autoClose: true,
+				addCloseButton: false,
+				type: 'info'
+			}
+		]
+	);
+
+	$("#generateEvent").on('click',function(){
+		var framework = $("input[name=uiFramework]:checked").val();
+		var type = $("input[name=eventType]:checked").val();
+		
+		$(this).trigger(framework + '.' + type);
 	});
 });
